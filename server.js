caaa16565a9c2cf88d62f5abd328b84a16e1fa5c
Aditya <47073054+Adityaphalswal@users.noreply.github.com>
2023-04-05 10:33:38 +0530

    Update server.js
    updated path

--- server.js
+++ server.js
@@ -26,11 +26,12 @@ app.use("/api/message", messageRoutes);
 // ------------------Deployment------------------
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "https://chugli.netlify.app")));
   console.log("backend is in production mode");
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname1, "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
